c6347aace8cb2158e5ea6284baef19cd065d7533
Casey Waweru <casey.waweru@gmail.com>
2026-06-18 11:47:52 +0300

    feat: defining structure of application

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,27 @@
 class Song:
-    pass
+
+    # class attributes
+    count
+    genres
+    artists
+    genre_count
+    artists_count
+
+
+    def __init__(self, name, artist, genre):
+        pass
+
+    def add_song_to_count():
+        pass
+
+    def add_to_genres():
+        pass
+
+    def add_to_artists():
+        pass
+
+    def add_to_genre_count():
+        pass
+
+    def add_to_artists_count():
+        pass
